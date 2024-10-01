db77adc73f8a67a7952aa8d5d46273dc5afb7794
Evi Ofekeze <eviofekeze@u.boisestate.edu>
2024-09-30 22:52:16 -0600

    corrected typo

--- swesarr_pytools/__init__.py
+++ swesarr_pytools/__init__.py
@@ -4,8 +4,6 @@ __author__ = """Evi Ofekeze"""
 __email__ = "eviofekeze@u.boisestate.edu"
 __version__ = "0.1.1"
 
-from tests.test_access_swesarr import access_swesarr
-
 from .access_swesarr import AccessSwesarr
 from .data_tools import ReadSwesarr, ReadLidar, SwesarrLidarProjection, combine_swesarr_lidar
 
